f3af3afd3ccd625620e14d84987d93cd0f683f15
firilka <firilka000@mail.ru>
2026-05-19 21:38:35 +0800

    Added DB(tables, context)

---
+++ AcademyJournal.Core/Models/Enums/GradeType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcademyJournal.Core.Models.Enums
+{
+    public enum GradeType //Типы оценок
+    {
+        lesson, //за урок
+        homework, //за дз
+        exam, //за экзамен
+        final //итоговая
+    }
+}
--- AcademyJournal.Core/Models/Module.cs
+++ AcademyJournal.Core/Models/Module.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace AcademyJournal.Core.Models
 {
-    internal class Module
+    public class Module
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Grade> Grades { get; set; } = new List<Grade>();
+
+        public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+
     }
 }
