6dedd06a0288bf0abd6241eebde4f582a89df559
Софья <cjamz15082009@gmail.com>
2026-05-21 19:00:58 +0800

    стиль

--- AcademyJournal.Core/obj/Debug/net10.0/AcademyJournal.Core.AssemblyInfo.cs
+++ AcademyJournal.Core/obj/Debug/net10.0/AcademyJournal.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AcademyJournal.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edd921d77bd1a2c16949c1c4aef1b63e7d28f0c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+face930666b6c4dccd6d3455ff10c40f47b7899f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcademyJournal.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcademyJournal.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
