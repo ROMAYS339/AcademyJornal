31a0a892c7fdcdd4ba2dd28f254781a7d7c3675d
firilka <firilka000@mail.ru>
2026-05-28 12:30:44 +0800

    Added GPA in last column

--- AcademyJornal.UI/Views/GradesInputView.xaml.cs
+++ AcademyJornal.UI/Views/GradesInputView.xaml.cs
@@ -38,30 +38,51 @@ namespace AcademyJornal.UI.Views
             // 2. Находим, какое максимальное количество оценок есть у кого-то из студентов
             int maxGradesCount = students.Max(s => s.grades?.Count ?? 0);
 
-            // 3. Генерируем новые колонки в столбик
+            // 3. Генерируем новые колонки оценок в столбик
             for (int i = 0; i < maxGradesCount; i++)
             {
                 var gradeColumn = new DataGridTextColumn
                 {
                     Header = $"Оценка {i + 1}",
-                    // Привязываемся к конкретному индексу в списке 'grades'
-                    Binding = new Binding($"grades[{i}]"),
+                    // Важно: привязываемся через свойство Student, так как мы изменим ItemsSource ниже
+                    Binding = new Binding($"Student.grades[{i}]"),
                     Width = new DataGridLength(80),
                 };
 
-                // Центрируем текст оценки внутри ячейки и делаем его жирным
                 var textStyle = new Style(typeof(TextBlock));
                 textStyle.Setters.Add(new Setter(TextBlock.TextAlignmentProperty, TextAlignment.Center));
                 textStyle.Setters.Add(new Setter(TextBlock.HorizontalAlignmentProperty, HorizontalAlignment.Center));
                 textStyle.Setters.Add(new Setter(TextBlock.FontWeightProperty, FontWeights.Bold));
                 gradeColumn.ElementStyle = textStyle;
 
-                // Добавляем колонку в таблицу
                 GradeGrid.Columns.Add(gradeColumn);
             }
 
-            // 4. Обновляем данные в таблице
-            GradeGrid.ItemsSource = students;
+            // 4. ДОПИСЫВАЕМ СТОЛБЕЦ СРЕДНЕГО БАЛЛА
+            var avgColumn = new DataGridTextColumn
+            {
+                Header = "Средний балл",
+                Binding = new Binding("Average") { StringFormat = "F1" }, // Округляем до 1 знака
+                Width = new DataGridLength(110)
+            };
+
+            var avgStyle = new Style(typeof(TextBlock));
+            avgStyle.Setters.Add(new Setter(TextBlock.TextAlignmentProperty, TextAlignment.Center));
+            avgStyle.Setters.Add(new Setter(TextBlock.HorizontalAlignmentProperty, HorizontalAlignment.Center));
+            avgStyle.Setters.Add(new Setter(TextBlock.FontWeightProperty, FontWeights.Bold));
+            avgStyle.Setters.Add(new Setter(TextBlock.ForegroundProperty, Application.Current.Resources["AccentPurpleLightBrush"]));
+            avgColumn.ElementStyle = avgStyle;
+
+            GradeGrid.Columns.Add(avgColumn);
+
+            // 5. ДОПИСЫВАЕМ ПОДСЧЕТ СРЕДНЕГО ЗНАЧЕНИЯ НА ЛЕТУ
+            // Передаем в таблицу анонимный объект, где средний балл уже вычислен через LINQ
+            GradeGrid.ItemsSource = students.Select(s => new
+            {
+                fullName = s.fullName, // Для первой колонки "Студент"
+                Student = s,           // Для динамических колонок оценок
+                Average = s.grades != null && s.grades.Count > 0 ? s.grades.Average() : 0.0
+            }).ToList();
         }
     }
 }
